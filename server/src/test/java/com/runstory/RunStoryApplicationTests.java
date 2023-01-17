fabb41dedd59dc5578f2b456791481248140bff8
tykwon97 <tykwon97@naver.com>
2023-01-17 16:40:37 +0900

    initial project

---
+++ server/src/test/java/com/runstory/RunStoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.runstory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunStoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
