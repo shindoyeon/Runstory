c210d37d7eb058a1164c6e03c3122b8ef0705472
dyeon7310 <dyeon7310@gmail.com>
2023-01-24 19:32:27 +0900

    [add] User 관련 Entity 생성
    Former-commit-id: 026d7f9eedf7c85ac16763f9c78ef5675913c254

---
+++ server/src/main/java/com/runstory/domain/user/entity/Follow.java
@@ -0,0 +1,27 @@
+package com.runstory.domain.user.entity;
+
+import lombok.Data;
+import org.hibernate.annotations.Comment;
+import org.hibernate.annotations.DynamicInsert;
+
+import javax.persistence.*;
+
+import static javax.persistence.FetchType.LAZY;
+
+@Entity
+@Data
+@DynamicInsert
+public class Follow {
+
+    @Id
+    @GeneratedValue
+    private Long followId;
+    @Comment("팔로워")
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name="from_user_id", foreignKey = @ForeignKey(name="fk_from_user_id_user_user_id"))
+    private User from;
+    @Comment("팔로잉")
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name="to_user_id", foreignKey = @ForeignKey(name="fk_to_user_id_user_user_id"))
+    private User to;
+}
