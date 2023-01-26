d3ee02462a07b49d2349e3be1ab36bcc8697ddff
minsoo <imminsoo97@gmail.com>
2023-01-26 10:09:23 +0900

    feat : "hashtag 수정"
    Former-commit-id: 66a3ecdc79814c03e05a40a0e1ba6f60ca7f1233

--- server/src/main/java/com/runstory/domain/hashtag/FeedHashtag.java
+++ server/src/main/java/com/runstory/domain/hashtag/FeedHashtag.java
@@ -1,10 +1,7 @@
 package com.runstory.domain.hashtag;
 
-
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.runstory.domain.feed.Feed;
-import com.runstory.domain.running.Running;
-import com.runstory.domain.user.entity.User;
 import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.CascadeType;
@@ -22,29 +19,29 @@ import lombok.Data;
 import org.hibernate.annotations.Comment;
 import org.hibernate.annotations.DynamicInsert;
 
-
 @Entity
 @Data
 @DynamicInsert
-public class SelectedHashtag {
+public class FeedHashtag {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @JsonManagedReference
     @OneToMany
-    @JoinColumn(name = "selected_hashtag_id")
-    @Comment("선택된 해시태그 아이디")
+    @JoinColumn(name = "feed_hashtag_id")
+    @Comment("피드 해시태그 아이디")
     private List<HashTag> hashTags = new ArrayList<>();
-
     @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JoinColumn(name = "hashtag_id",
-        foreignKey = @ForeignKey(name = "fk_selected_hashtag_to_hashtag"))
+        foreignKey = @ForeignKey(name = "fk_feed_hashtag_to_hashtag"))
     @Comment("해시태그 아이디")
     private HashTag hashtag;
     @Column(length=30, nullable = false)
     @Comment("해시태그 종류")
     private String hashtagType;
-
-
-
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "feed_id",
+        foreignKey = @ForeignKey(name = "fk_feed_hashtag_to_feed"))
+    @Comment("피드아이디")
+    private Feed feedId;
 }
--- server/src/main/java/com/runstory/domain/hashtag/UserHashtag.java
+++ server/src/main/java/com/runstory/domain/hashtag/UserHashtag.java
@@ -1,9 +1,6 @@
 package com.runstory.domain.hashtag;
 
-
 import com.fasterxml.jackson.annotation.JsonManagedReference;
-import com.runstory.domain.feed.Feed;
-import com.runstory.domain.running.Running;
 import com.runstory.domain.user.entity.User;
 import java.util.ArrayList;
 import java.util.List;
@@ -22,29 +19,29 @@ import lombok.Data;
 import org.hibernate.annotations.Comment;
 import org.hibernate.annotations.DynamicInsert;
 
-
 @Entity
 @Data
 @DynamicInsert
-public class SelectedHashtag {
+public class UserHashtag {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @JsonManagedReference
     @OneToMany
-    @JoinColumn(name = "selected_hashtag_id")
-    @Comment("선택된 해시태그 아이디")
+    @JoinColumn(name = "user_hashtag_id")
+    @Comment("유저 해시태그 아이디")
     private List<HashTag> hashTags = new ArrayList<>();
-
     @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JoinColumn(name = "hashtag_id",
-        foreignKey = @ForeignKey(name = "fk_selected_hashtag_to_hashtag"))
+        foreignKey = @ForeignKey(name = "fk_user_hashtag_to_hashtag"))
     @Comment("해시태그 아이디")
     private HashTag hashtag;
     @Column(length=30, nullable = false)
     @Comment("해시태그 종류")
     private String hashtagType;
-
-
-
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id",
+        foreignKey = @ForeignKey(name = "fk_user_hashtag_to_user"))
+    @Comment("유저아이디")
+    private User userId;
 }
