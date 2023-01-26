48ef7e9fbb6e05a3ac0491173ad1587ffe62f663
tykwon97 <tykwon97@naver.com>
2023-01-26 14:20:51 +0900

    [Feat] User Controller Swagger Test
    Former-commit-id: 17cd4df1e9fe5d3a08a607122ca973395d246011

--- server/src/main/java/com/runstory/api/controller/UserController.java
+++ server/src/main/java/com/runstory/api/controller/UserController.java
@@ -5,6 +5,10 @@ import com.runstory.common.model.response.BaseResponseBody;
 import com.runstory.domain.user.entity.User;
 import com.runstory.service.UserService;
 //import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -17,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * 유저 관련 API 요청 처리를 위한 컨트롤러 정의.
  */
-//@Api(value = "유저 API", tags = {"User"})
+@Api(value = "유저 API", tags = {"User"})
 @RestController
 @RequestMapping("/api/v1/users")
 public class UserController {
@@ -26,13 +30,13 @@ public class UserController {
 	UserService userService;
 	
 	@PostMapping()
-//	@ApiOperation(value = "회원 가입", notes = "<strong>아이디와 패스워드</strong>를 통해 회원가입 한다.")
-//    @ApiResponses({
-//        @ApiResponse(code = 200, message = "성공"),
-//        @ApiResponse(code = 401, message = "인증 실패"),
-//        @ApiResponse(code = 404, message = "사용자 없음"),
-//        @ApiResponse(code = code = 500, message = "서버 오류")
-//    })
+	@ApiOperation(value = "회원 가입", notes = "<strong>아이디와 패스워드</strong>를 통해 회원가입 한다.")
+    @ApiResponses({
+        @ApiResponse(code = 200, message = "성공"),
+        @ApiResponse(code = 401, message = "인증 실패"),
+        @ApiResponse(code = 404, message = "사용자 없음"),
+        @ApiResponse(code = 500, message = "서버 오류")
+    })
 	public ResponseEntity<? extends BaseResponseBody> register(
 			@RequestBody UserRegisterPostReq registerInfo) { //@ApiParam(value="회원가입 정보", required = true)
 		
