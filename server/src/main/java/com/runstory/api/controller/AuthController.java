e775ab50fccdcd48e31e5988a853fe56a87c3395
tykwon97 <tykwon97@naver.com>
2023-01-27 15:57:21 +0900

    [Refactor] 이메일 인증 코드 전송 API
    Former-commit-id: cfc2ce740b1dac0aa5473e61255727b2c8c1bf25

--- server/src/main/java/com/runstory/api/controller/AuthController.java
+++ server/src/main/java/com/runstory/api/controller/AuthController.java
@@ -31,16 +31,13 @@ public class AuthController {
 
 	@Autowired
 	AuthService authService;
-	
-//	@Autowired
-//	PasswordEncoder passwordEncoder;
 
 	@GetMapping("/email")
 	@ApiOperation(value = "이메일 인증 코드 전송", notes = "<strong>제공 받은 이메일에</strong> 인증 코드를 전송한다.")
     @ApiResponses({
-//        @ApiResponse(code = 200, message = "성공", response = UserLoginPostRes.class),
-        @ApiResponse(code = 401, message = "인증 실패", response = BaseResponseBody.class),
-        @ApiResponse(code = 404, message = "사용자 없음", response = BaseResponseBody.class),
+        @ApiResponse(code = 200, message = "성공", response = BaseResponseBody.class),
+//        @ApiResponse(code = 401, message = "인증 실패", response = BaseResponseBody.class),
+        @ApiResponse(code = 404, message = "이메일 전송 실패", response = BaseResponseBody.class),
         @ApiResponse(code = 500, message = "서버 오류", response = BaseResponseBody.class)
     })
 	public ResponseEntity<?> emailSend(@ApiParam(value="이메일 정보", required = true) @RequestParam String userEmail) {
@@ -48,44 +45,8 @@ public class AuthController {
 		try {
 			String confirm = authService.sendSimpleMessage(userEmail);
 			return ResponseEntity.ok(BaseResponseBody.of(200, confirm));
-		}catch (Exception e){
-			return ResponseEntity.ok(null);
+		} catch (Exception e) {
+			return ResponseEntity.ok(BaseResponseBody.of(500,"BAD REQUEST"));
 		}
-
-//		String userId = loginInfo.getId();
-//		String password = loginInfo.getPassword();
-//
-//		User user = userService.getUserByUserId(userId);
-//		// 로그인 요청한 유저로부터 입력된 패스워드 와 디비에 저장된 유저의 암호화된 패스워드가 같은지 확인.(유효한 패스워드인지 여부 확인)
-//		if(passwordEncoder.matches(password, user.getPassword())) {
-//			// 유효한 패스워드가 맞는 경우, 로그인 성공으로 응답.(액세스 토큰을 포함하여 응답값 전달)
-//			return ResponseEntity.ok(UserLoginPostRes.of(200, "Success", JwtTokenUtil.getToken(userId)));
-
-//		return ResponseEntity.ok(200);
-//		}
-//		// 유효하지 않는 패스워드인 경우, 로그인 실패로 응답.
-//		return ResponseEntity.status(401).body(UserLoginPostRes.of(401, "Invalid Password", null));
 	}
-	
-//	@PostMapping("/login")
-//	@ApiOperation(value = "로그인", notes = "<strong>아이디와 패스워드</strong>를 통해 로그인 한다.")
-//    @ApiResponses({
-//        @ApiResponse(code = 200, message = "성공", response = UserLoginPostRes.class),
-//        @ApiResponse(code = 401, message = "인증 실패", response = BaseResponseBody.class),
-//        @ApiResponse(code = 404, message = "사용자 없음", response = BaseResponseBody.class),
-//        @ApiResponse(code = 500, message = "서버 오류", response = BaseResponseBody.class)
-//    })
-//	public ResponseEntity<UserLoginPostRes> login(@RequestBody @ApiParam(value="로그인 정보", required = true) UserLoginPostReq loginInfo) {
-//		String userId = loginInfo.getId();
-//		String password = loginInfo.getPassword();
-//
-//		User user = userService.getUserByUserId(userId);
-//		// 로그인 요청한 유저로부터 입력된 패스워드 와 디비에 저장된 유저의 암호화된 패스워드가 같은지 확인.(유효한 패스워드인지 여부 확인)
-//		if(passwordEncoder.matches(password, user.getPassword())) {
-//			// 유효한 패스워드가 맞는 경우, 로그인 성공으로 응답.(액세스 토큰을 포함하여 응답값 전달)
-//			return ResponseEntity.ok(UserLoginPostRes.of(200, "Success", JwtTokenUtil.getToken(userId)));
-//		}
-//		// 유효하지 않는 패스워드인 경우, 로그인 실패로 응답.
-//		return ResponseEntity.status(401).body(UserLoginPostRes.of(401, "Invalid Password", null));
-//	}
 }
