f4d81694c22f91ee66b54163a03ce618e6395502
dyeon7310 <dyeon7310@gmail.com>
2023-01-28 23:27:49 +0900

    [Add] Feed 개인 조회 추가(querydsl 사용)
    Former-commit-id: b4de42893da581f317885e300c8f3ec6029aa657

--- server/src/main/java/com/runstory/repository/FeedRepository.java
+++ server/src/main/java/com/runstory/repository/FeedRepository.java
@@ -2,9 +2,14 @@ package com.runstory.repository;
 
 import com.runstory.domain.feed.entity.Feed;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface FeedRepository extends JpaRepository<Feed, Long> {
     List<Feed> findAll();
+
+    @Query("select f from Feed f where f.user.userSeq= :userId")
+    List<Feed> findByUserId(Long userId);
+
 }
--- server/src/main/java/com/runstory/service/FeedService.java
+++ server/src/main/java/com/runstory/service/FeedService.java
@@ -1,20 +1,53 @@
 package com.runstory.service;
 
+import com.runstory.domain.feed.dto.FeedDto;
 import com.runstory.domain.feed.entity.Feed;
+import com.runstory.domain.user.entity.Follow;
 import com.runstory.repository.FeedRepository;
+import com.runstory.repository.FeedRepositoryCustom;
+import com.runstory.repository.FollowRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class FeedService {
     private final FeedRepository feedRepository;
+    private final FeedRepositoryCustom feedRepositoryCustom;
+    private final FollowRepository followRepository;
 
-    public List<Feed> find(){
-        return feedRepository.findAll();
+    public List<FeedDto> findAll(){
+        List<Feed> feeds = feedRepository.findAll();
+        List<FeedDto> result = feeds.stream().map(f->new FeedDto(f)).collect(Collectors.toList());
+        return result;
     }
+
+    public List<FeedDto> findByUserId(Long myUserId, Long yourUserId, Boolean isMe){
+        //팔로우 관계인 경우->전체공개, 팔로우 공개 게시물 조회
+        //팔로우 관계가 아닌 경우->전체공개 게시물만 조회
+        List<Feed> feeds = new ArrayList<>();
+        if(isMe){
+            feeds = feedRepository.findByUserId(myUserId);
+        }else{
+            //팔로우 관계인지 조회
+            Follow follow = followRepository.findFollow(myUserId, yourUserId);
+            //팔로우 관계 아님->전체공개 게시물만
+            if(follow==null)
+                feeds = feedRepositoryCustom.searchByUserId(yourUserId, false);
+            //전체공개+팔로우공개 게시물
+            else{
+                feeds = feedRepositoryCustom.searchByUserId(yourUserId, true);
+            }
+        }
+
+        List<FeedDto> result = feeds.stream().map(f->new FeedDto(f)).collect(Collectors.toList());
+        return result;
+    }
+
 }
