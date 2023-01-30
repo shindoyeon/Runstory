1a8d966e00c17dedae01b446f7d8d26774c823a0
gs20wow@gmail.com <gs20wow@gmail.com>
2023-01-31 02:25:41 +0900

    [Add] 프로필 사진 첨부 수정
    Former-commit-id: e0eefea342a1f2fd874f310b4537c180f23717c1

--- client/runtogether/src/Feed/ProfileIdPhoto.js
+++ client/runtogether/src/Feed/ProfileIdPhoto.js
@@ -1,33 +1,35 @@
-import React, {useRef} from 'react';
-import { Image, Card, IconButton, Img } from '@chakra-ui/react';
+import React, {useRef, useState} from 'react';
+import { Card, IconButton } from '@chakra-ui/react';
 import { SmallAddIcon } from '@chakra-ui/icons'
 import Imgfile from './권태윤.png';
 import './ProfileIdPhoto.css';
 
 const ProfileIdPhoto =({
-  onChange,
-  src
 })=>
-  <label htmlFor="photo-upload" className="custom-file-upload fas">
+<label htmlFor="photo-upload" className="custom-file-upload fas">
     <div className="img-wrap img-upload" >
-      <img for="photo-upload" src={Imgfile}/>
+      <img for="photo-upload" src={'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png'}/>
     </div>
-    <IconButton 
-      className='photo-upload'
-      type="file" 
-      icon={<SmallAddIcon/>} 
-      onClick={onChange}/>
-  </label>
-
-
-// 이미지를 변경하는 버튼 구현  
-// 버튼 클릭하면 이미지들 볼 수 있고,
-// 입력하면 프로필에 이미지 등록
+    <IconButton className='photo-upload' type="file" icon={<SmallAddIcon/>} 
+    onClick={fileInput.current.click()}/>
+    </label>
+    
+    
+    
+    // 이미지를 변경하는 버튼 구현  
+    // 버튼 클릭하면 이미지들 볼 수 있고,
+    // 입력하면 프로필에 이미지 등록
 function ChangeProfileIdPhoto(){
-  const photoUpload = e =>{
+  const fileInput = useRef(null)
+  const [Image, setImage] = useState("https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png")
+  const photoUpload = (e) =>{
     e.preventDefault();
-    const reader = new FileReader();
-    const file = e.target.files[0];
+    var reader = new FileReader();
+    var file = e.target.files[0];
+    reader.onload = () => {
+      setImage(reader.result)
+    };
+    reader.readAsDataURL(file);
   }
   
   return(
@@ -35,7 +37,6 @@ function ChangeProfileIdPhoto(){
     <Card derection={{base:'column'}}>
       <ProfileIdPhoto
         onChange={photoUpload} 
-        src={Imgfile}
         alt='Tae yoon'  
       />
     </Card>
