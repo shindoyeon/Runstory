e106d2119c521d2e1490f4108635e2bc4a3a17cd
songheewon <songheew1020@gmail.com>
2023-02-02 15:48:35 +0900

    [BUG] syntax error
    Former-commit-id: 89ba8e2bf655e918346ace804f5864332fd9a5fb

--- client/runtogether/src/MainPage/Recommend.js
+++ client/runtogether/src/MainPage/Recommend.js
@@ -1,23 +1,23 @@
-import React, {useEffect, useState} from "react";
-import Slider from "react-slick";
-import axios from 'axios';
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-// import { Swiper, SwiperSlide } from 'swiper/react';
-// import "swiper/swiper.scss";
-// import 'swiper/css';
-// import SwiperCore, { Navigation, Pagination } from "swiper";
-// import "swiper/css"; //basic
-import './Recommend.css';
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faCircle, faCircleDot } from "@fortawesome/free-regular-svg-icons";
+// import React, {useEffect, useState} from "react";
+// import Slider from "react-slick";
+// import axios from 'axios';
+// import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
+// // import { Swiper, SwiperSlide } from 'swiper/react';
+// // import "swiper/swiper.scss";
+// // import 'swiper/css';
+// // import SwiperCore, { Navigation, Pagination } from "swiper";
+// // import "swiper/css"; //basic
+// import './Recommend.css';
+// // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// // import { faCircle, faCircleDot } from "@fortawesome/free-regular-svg-icons";
 
-const Recommend = ({runningCrew}) => {
+// const Recommend = ({runningCrew}) => {
   
     
-  return (
+//   return (
     
-    );
-}
+//     );
+// }
 
-export default Recommend;
+// export default Recommend;
