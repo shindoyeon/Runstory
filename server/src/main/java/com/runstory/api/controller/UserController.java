b9293b4035d0b702f8befe80d9d1b004587f1861
tykwon97 <tykwon97@naver.com>
2023-02-02 21:27:24 +0900

    [Feat] CrossOrigin annotation 추가
    Former-commit-id: 306cde3b4d1c1ea928e359e81cc166ccd178098e

--- server/src/main/java/com/runstory/api/controller/UserController.java
+++ server/src/main/java/com/runstory/api/controller/UserController.java
@@ -18,6 +18,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -34,6 +35,7 @@ import springfox.documentation.annotations.ApiIgnore;
  */
 @Api(value = "유저 API", tags = {"User"})
 @RestController
+@CrossOrigin
 @RequestMapping("/user")
 public class UserController {
 	
