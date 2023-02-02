920fbcef70f4be5ac2d19f43ec28628e6efc1151
songheewon <songheew1020@gmail.com>
2023-02-02 10:53:10 +0900

    [FEAT] 게시글 길이에 따른 클릭 이벤트 설정
    Former-commit-id: 59eacfc77a5f2d4ad99311cd622bb86e85a890ae

--- client/runtogether/src/MainPage/FeedCard.js
+++ client/runtogether/src/MainPage/FeedCard.js
@@ -2,23 +2,32 @@ import {
     Card, // chakra-ui의 Card로 피드 하나를 구성할 것임 
     CardHeader,
     Image,
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    ModalCloseButton,
-    ModalBody,
-    ModalFooter,
-    useDisclosure,
-    Input,
-    Button,
-    CardBody,
-    ChakraProvider,
-    Spinner,
+    Divider
   } from '@chakra-ui/react';
+  import React, {useState} from 'react';
+  import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; // fontawesome 사용
+  import { faShare, faHeart, faArrowRotateRight } from "@fortawesome/free-solid-svg-icons";
+  import { faComment } from "@fortawesome/free-regular-svg-icons";
   import './Feed.css';
 
-  function FeedCard({feed}) {
+  function FeedCard(props) {
+    const feed = props.feed;
+    const clickLike = id => {
+        console.log(id)
+  }
+
+  function moreContent(e) {
+    if(e.target.classList.contains("feed-content-open")) {
+        e.target.classList.remove('feed-content-open');
+        e.target.classList.add('feed-content');
+    }
+    else {
+        e.target.classList.remove('feed-content')
+        e.target.classList.add('feed-content-open')
+    }
+  }
+
+
     return (
     <div height="50vh" margin='0 auto'>
         <Card className='card' variant='outline' boxShadow='rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px;'>
@@ -36,7 +45,6 @@ import {
                     </CardHeader>
                     {/* 피드 내용 */}
                     <div className='card-body'>
-                        {console.log(feed.feedFiles)}
                         <Image
                                 border='1px solid #CBD9E7'
                                 margin='0 auto'
@@ -44,19 +52,19 @@ import {
                                 width='90%'
                                 borderRadius='lg'
                                 src={feed.feedFiles}
-                        />
-                        {/* <div className='post-image' backgroundImage={item.contentImg}></div> */}
-                            
+                        /> 
                             {/* 내용 */}
-                            <div className="feed-content">{feed.content}</div>
-                    {/* <div className='like-comment'> 
-                        {item.isLiked ?
+                        {/* <div className="feed-content">{feed.content}</div> */}
+                        <div className='feed-content' onClick={moreContent}>{feed.content}</div>
+                    <div className='like-comment'> 
+                        <Divider></Divider>
+                        {feed.isLiked ?
                         <FontAwesomeIcon className='like' icon={faHeart} style={{ color: 'red', fontSize: '25px', fontWeight: 'bold'}} onClick={()=> {
-                            clickLike(idx)}}/> :    //꽉차있는 하트를 return
+                            clickLike(feed.feedId)}}/> :    //꽉차있는 하트를 return
                         <FontAwesomeIcon className='like' icon={faHeart} style={{ color: 'grey', fontSize: '25px'}} onClick={()=> {
-                            clickLike(idx)}}/>}
-                        <FontAwesomeIcon className='comment' icon={faComment} style={{ fontSize: '25px'}} onClick={onOpen}/>
-                    </div> */}
+                            clickLike(feed.feedId)}}/>}
+                        {/* <FontAwesomeIcon className='comment' icon={faComment} style={{ fontSize: '25px'}} onClick={onOpen}/> */}
+                    </div>
                 </div>
                     {/* 좋아요 및 댓글 버튼 */}
             </Card>
