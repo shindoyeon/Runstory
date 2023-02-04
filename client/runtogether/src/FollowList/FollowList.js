dbfcc5d01b886c67517632bd6e8645de7e00530d
gs20wow <gs20wow@gmail.com>
2023-02-04 20:45:49 +0900

    [Fix] 팔로우 UI 구현
    Former-commit-id: ea4fe6ae6abc773caa83fc0ed8c7531b1e038c95

---
+++ client/runtogether/src/FollowList/FollowList.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Header from '../common/Header';
+import Footer from '../common/Footer';  
+import Followers from './FollowerList';
+import Followings from './FollowingList';
+import FollowListPageMsg from './FollowListPageMsg'
+import { 
+  ChakraProvider,
+  Tabs,
+   TabList,
+   TabPanels, 
+   Tab, 
+   TabPanel } from '@chakra-ui/react'
+  
+const FollowPage= () => {
+  return (
+    <ChakraProvider>
+      <Header></Header>
+      <FollowListPageMsg></FollowListPageMsg>
+      <Tabs variant='enclosed'>
+      <TabList>
+        <Tab>팔로워</Tab>
+        <Tab>팔로잉</Tab>
+      </TabList>
+      <TabPanels>
+        <TabPanel>
+          <Followers/>
+        </TabPanel>
+        <TabPanel>
+          <Followings/>
+        </TabPanel>
+      </TabPanels>
+      </Tabs>
+      <Footer></Footer>
+    </ChakraProvider>
+  )
+}
+
+export default FollowPage;
+
