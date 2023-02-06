26d6ec113f74c7c4b3c1e5a2a526f1e4d010e525
tykwon97 <tykwon97@naver.com>
2023-02-06 14:39:45 +0900

    [Feat] STOMP TEST
    Former-commit-id: c608d9ca9992b4eaa38edd2804fa1ee7b62296fd

---
+++ server/src/main/java/com/runstory/api/controller/ChatController.java
@@ -0,0 +1,31 @@
+package com.runstory.api.controller;
+
+import com.runstory.domain.chat.dto.ChatMessageDto;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by rajeevkumarsingh on 24/07/17.
+ */
+@Controller
+public class ChatController {
+
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public ChatMessageDto sendMessage(@Payload ChatMessageDto chatMessage) {
+        return chatMessage;
+    }
+
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/public")
+    public ChatMessageDto addUser(@Payload ChatMessageDto chatMessage,
+                               SimpMessageHeaderAccessor headerAccessor) {
+        // Add username in web socket session
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        return chatMessage;
+    }
+
+}
