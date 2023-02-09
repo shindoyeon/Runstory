aef3436b6c967ea7e3e4af832ac5fbc3906730fd
gs20wow <gs20wow@gmail.com>
2023-02-09 17:08:55 +0900

    [Fix] 프로필/팔로워 수정
    Former-commit-id: 5fd34649ff693588a55a7d24eeea730e746f820e

---
+++ client/runtogether/src/Feed/ProfileFollower.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import {
+  ChakraProvider,
+  Card,
+} from '@chakra-ui/react';
+import 'ProfileFollower.css'
+
+// 나를 팔로워하는 사람들
+const Profilefollwer = () => {
+
+  // const ProFollower :  
+  return (
+    <ChakraProvider>
+    <Card style={{}} direction={{base:column}}>
+      <div>
+      팔로워
+      </div>
+      <br/>
+      0 명
+    </Card>
+    </ChakraProvider>
+    )
+  }
+
+export default Profilefollwer;
