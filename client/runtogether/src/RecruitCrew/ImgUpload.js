bff34ef7eb6c80b7b33b61a0338fdb679c497da9
songheewon <songheew1020@gmail.com>
2023-02-10 11:37:52 +0900

    [FIX] 이미지 URL 수정
    Former-commit-id: 05b8a9a00b637d36ddb0fd8c92e64bcd623fa6c6

--- client/runtogether/src/RecruitCrew/ImgUpload.js
+++ client/runtogether/src/RecruitCrew/ImgUpload.js
@@ -11,6 +11,10 @@ const ImgUpload = () => {
         fileInput.current.click();
     };
 
+    function deleteImg() {
+      console.log("삭제")
+    }
+
     const handleChange = e => {
       var reader = new FileReader();
       reader.readAsDataURL(e.target.files[0]);
@@ -28,22 +32,25 @@ const ImgUpload = () => {
         var previewBox = document.getElementById('preview-box');
         previewBox.style.display = 'inline';
         previewBox.appendChild(preview);
-        var deleteImg = document.createElement('div')
-        deleteImg.textContent = '삭제';
-        deleteImg.id = preview.src;
-        deleteImg.style.borderRadius = "20px";
-        deleteImg.style.backgroundColor = "#EEB6B6";
-        deleteImg.style.textAlign = "center";
-        deleteImg.style.width = "10%";
-        deleteImg.style.height = "20px";
-        deleteImg.style.lineHeight = "20px";
-        deleteImg.style.fontSize = "14px";
-        deleteImg.style.color = "#6A6A6A";
-        deleteImg.style.margin = "0 auto";
-        deleteImg.style.marginBottom = "20px";
-        previewBox.appendChild(deleteImg);
+        // var deleteImg = document.createElement('div')
+        // deleteImg.textContent = '삭제';
+        // deleteImg.id = preview.src;
+        // deleteImg.style.borderRadius = "20px";
+        // deleteImg.style.fontWeight = "bold";
+        // deleteImg.style.backgroundColor = "#EEB6B6";
+        // deleteImg.style.textAlign = "center";
+        // deleteImg.style.width = "10%";
+        // deleteImg.style.height = "20px";
+        // deleteImg.style.lineHeight = "20px";
+        // deleteImg.style.fontSize = "14px";
+        // deleteImg.style.color = "#6A6A6A";
+        // deleteImg.style.margin = "0 auto";
+        // deleteImg.style.marginBottom = "20px";
+        // deleteImg.onclick(deleteImg());
+        // previewBox.appendChild(deleteImg);
       }
     };
+
   return (
     <React.Fragment>
       <div className='preview-box' id='preview-box' style={{display: 'none'}}></div>
