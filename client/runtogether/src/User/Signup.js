d244ae9a7fca9a62326f8de77a686e257817e718
dyeon7310 <dyeon7310@gmail.com>
2023-02-12 02:43:39 +0900

    [Fix] axios 추가에 따른 변경
    Former-commit-id: b6743c0a5257b2577c98742762a23783fb4ef3ea

--- client/runtogether/src/User/Signup.js
+++ client/runtogether/src/User/Signup.js
@@ -1,7 +1,7 @@
 import { useRef, useState, useEffect } from "react";
 import { faCheck, faTimes, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import axios from './api/axios';
+import axios from '../api/axios';
 import './Signup.css'
 import {
     ChakraProvider,
