c7c332cbc556746d1da817d9d9b61cc8d20a2e4d
dyeon7310 <dyeon7310@gmail.com>
2023-02-13 15:03:24 +0900

    [Fix] 카카오 로그인 매핑 url 변경
    Former-commit-id: caaf8f4bcea528431d1d64308b2385c13a5ac7da

--- client/runtogether/src/User/OAuthRedirectHandler.js
+++ client/runtogether/src/User/OAuthRedirectHandler.js
@@ -9,7 +9,7 @@ const OAuth2RedirectHandler = () => {
     try {
       var code = new URL(window.location.href).searchParams.get("code");
       console.log(code);
-      const response = await axios.get(`http://localhost:8080/api/auth/login/kakao?code=${code}`);
+      const response = await axios({url: `/auth/login/kakao?code=${code}`, method: "GET"});
       const ACCESS_TOKEN = response.data.data.accessToken;
       localStorage.setItem("access-token", ACCESS_TOKEN);
       navigate("/");
