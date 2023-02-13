7c7622d1317ae0774af15bfb2bc09cb776e25aaf
Diligent0924 <sdc00039@gmail.com>
2023-02-13 14:03:14 +0900

    [FIX] RunngingDetail 참가인원 로직 및 Frontend Data 연결 완료
    Former-commit-id: 51f5fed7d6e158daa2cd5be3916bd602402d3f97

--- client/runtogether/src/RunningDetail/RunningDetail.js
+++ client/runtogether/src/RunningDetail/RunningDetail.js
@@ -51,9 +51,6 @@ function RunningDetail(){
       <Header></Header>
         <div style={{marginTop:"15%", marginLeft:"6%", marginRight:"6%"}}>
             <div>
-                {runnings.crewName}
-            </div>
-            <div>
             <p>러닝 크루 상세</p>
             </div>
             <div style={{  position: "relative", width: "300px", height: "200px", overflow: "hidden", textAlign : "center"}}>
@@ -80,7 +77,7 @@ function RunningDetail(){
             <div style={{marginTop:"15%", marginBottom:"8%"}}>
                 <HStack spacing='24px'>
                     <div>
-                        <Box w='170px' bg='teal.500' style={{ background: "white", borderBottom:"0px "}}> {runnings.runningContent}</Box>
+                        <Box w='170px' bg='teal.500' style={{ background: "white", borderBottom:"0px "}}> {runnings.crewName}</Box>
                         <p style={{borderTop:"0px"}}>{runnings.userId}</p>
                     </div>
                     <Spacer />
@@ -95,17 +92,17 @@ function RunningDetail(){
             </div>
             <div style={{marginTop:"10%"}}>
                 <p>{runnings.startLocation}</p>
-                <p>3/5</p>
+                <p>남자 : {runnings.man} / 여자 : {runnings.women} / 상관없음 : {runnings.total}</p>
                 <p>{runnings.startTime} - {runnings.endTime}</p>
             </div>
             <div style={{marginTop:"10%", marginBottom:"8%"}}>
                 <HStack spacing='24px'>
                     <Spacer />
-                    <BooleanRunning Something={runnings.runner} truevalue="예약취소" falsevalue= "예약하기" api={reservation}/>
-                    <BooleanRunning Something={runnings.dibs} truevalue="찜하기취소" falsevalue= "찜하기" api={dibsurl} />
+                    <BooleanRunning Something={runnings.runner} truevalue="예약취소" falsevalue= "예약하기" api={reservation} id = {runningId}/>
+                    <BooleanRunning Something={runnings.dibs} truevalue="찜하기취소" falsevalue= "찜하기" api={dibsurl} id = {runningId}/>
                 </HStack>
             </div>
-            <div style={{marginBottom:"15%"}}>
+            <div style={{marginBottom:"2%"}}>
                 <HStack spacing='24px'>
                     <Box w='70px' h='6' bg='teal.500' style={{textAlign:"center", background: "white", textDecoration:"underline" }}> 댓글  </Box>
                     <Spacer />
@@ -113,16 +110,19 @@ function RunningDetail(){
                 </HStack>
             </div>
             <div>
-                <HStack spacing='24px'>
-                    <Box w='70px' h='6' bg='teal.500' style={{textAlign:"center", background: "white", textDecoration:"underline" }}> 댓글  </Box>
-                    <Spacer />
-                    <Box w='70px' h='6' bg='teal.500' style={{textAlign:"center", background: "rgb(192,192,192)", paddingLeft: "3%", paddingRight:"3%", borderRadius:"30px"}}> + </Box>
-                </HStack>
-            </div>
-            <div style={{marginTop:"20px"}}>
                 {
                     comments.map(function(comment){
-                        return (<div style={{textAlign:"center", background: "grey", height:"100px", marginBottom:"20px"}}>{comment}</div>)
+                        return (
+                        <div style={{textAlign:"center", background: "grey", height:"100px", marginBottom:"20px", padding:"3%", background: "rgb(192,192,192)", borderRadius:"20px"}}>
+                            <HStack spacing='24px'>
+                                <Box w='150px' h='6' bg='teal.500' style={{background: "rgb(192,192,192)" }}> {comment.userId}  </Box>
+                                <Spacer />
+                                <Box w='400px' h='6' bg='teal.500' style={{background: "rgb(192,192,192)"}}> {comment.content} </Box>
+                                <Spacer />
+                                <Box w='100px' h='6' bg='teal.500' style={{textAlign:"center", background: "rgb(192,192,192)", paddingLeft: "3%", paddingRight:"3%", borderRadius:"30px"}}> {comment.regdate} </Box>
+                            </HStack>
+                        </div>
+                        )
                     })
                 }
             </div>
