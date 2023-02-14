aecb9246659574164fe57cae1005264a158b7b2f
dyeon7310 <dyeon7310@gmail.com>
2023-02-14 15:43:02 +0900

    [Fix] 메인페이지 조회 무한 스크롤 수정
    Former-commit-id: 0e66413a78b4d109ca84faeed8864b55dfa387b8

--- client/runtogether/src/MainPage/TempFeed.js
+++ client/runtogether/src/MainPage/TempFeed.js
@@ -67,7 +67,6 @@ export default function TempFeed() {
     const size = 1000;
     const lastfeedid = Number.MAX_SAFE_INTEGER + 1;
     (async () => {
-      // const data = null;
       if (localStorage.getItem("access-token") === null) {  //비회원 조회 시
         const data = await axios.all(
           [axios.get(
@@ -78,7 +77,6 @@ export default function TempFeed() {
           )
           ]);
           console.log(state.center);
-          console.log(data[0].data.data);
         setFeeds(data[0].data.data);
         setArr(Array.from(feeds.slice(startIdx, startIdx + 5)));
         setrunningCrew(data[1].data.data);
@@ -98,15 +96,9 @@ export default function TempFeed() {
           )
           ]);
           console.log(state.center);
-          console.log(data[0].data.data);
         setFeeds(data[0].data.data);
         setArr(Array.from(feeds.slice(startIdx, startIdx + 5)));
         setrunningCrew(data[1].data.data);
-      }
-
-      if (feeds.length === 0) {
-        setIsMore(false);
-        return;
       }
     })();
   }, [feeds.length]);
@@ -182,6 +174,12 @@ export default function TempFeed() {
       </div>
 
       <div className='entire-feed'>
+        {feeds.length===0?<div style={{ textAlign: "center", fontWeight: "light" }}>
+              <div>
+                모든 피드를 확인했습니다
+              </div>
+              <FontAwesomeIcon className='refresh' icon={faArrowRotateRight} onClick={refreshToHome}></FontAwesomeIcon>
+            </div>:
         <InfiniteScroll
           dataLength={arr.length}
           next={loadMore}
@@ -196,7 +194,6 @@ export default function TempFeed() {
             </div>
           }
         >
-
           {arr.map((feed, idx) => {
             return (
               <div height="50vh" margin='0 auto' marginTop='5%' key={idx}>
@@ -205,6 +202,7 @@ export default function TempFeed() {
             )
           })}
         </InfiniteScroll>
+        }
       </div>
     </>
   );
