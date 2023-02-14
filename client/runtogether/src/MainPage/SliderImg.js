7fb1213ac6ce32597ab78980bccb34a5a476ca0e
dyeon7310 <dyeon7310@gmail.com>
2023-02-14 10:41:14 +0900

    [Add] 메인페이지 러닝 크루 상세 조회 이동
    Former-commit-id: c8b6562813dd8e5934749ca9796b370cd75f7c17

--- client/runtogether/src/MainPage/SliderImg.js
+++ client/runtogether/src/MainPage/SliderImg.js
@@ -1,10 +1,13 @@
+import {NavLink} from "react-router-dom";
 function SliderImg({runningCrew}) {
     return (
         <>
             {
                 runningCrew.map((crew, idx) => {
                     return(
+                        <NavLink to={"/running/detail/" + crew.runningId}>
                             <img className='img' src={crew.imgFilePath} />
+                        </NavLink>
                         );
                       })
                 }
